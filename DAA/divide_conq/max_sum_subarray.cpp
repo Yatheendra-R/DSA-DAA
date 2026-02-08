3b26c3e029a32e682552b27386e83b7032770f09
Yatheendra-R <yatheendra.r07@gmail.com>
2026-02-08 17:26:55 +0530

    Update max_sum_subarray implementation

--- DAA/divide_conq/max_sum_subarray.cpp
+++ DAA/divide_conq/max_sum_subarray.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 
 using namespace std;
 int css(int arr[],int l,int h)
@@ -73,5 +73,65 @@ int main()
 
     int max_sum=find_max(arr,0,n-1);
     cout<<endl<<max_sum;
+    return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int css(vector <int> &arr,int start ,int mid, int last)
+{
+    int max_l=INT_MIN;
+    int max_r=INT_MIN;
+    int sum=0;
+    for(int i=mid;i>=start;i--)
+    {
+        sum+=arr[i];
+        if(sum>max_l)
+        {
+            max_l=sum;
+        }
+    }
+    sum=0;
+    for(int i=mid+1;i<=last;i++)
+    {
+        sum+=arr[i];
+        if(sum>max_r)
+        {
+            max_r=sum;
+        }
+    }
+    return max_r+max_l;
+}
+int max_sum(vector <int> &arr,int start , int last)
+{
+
+    if(start==last)
+    {
+
+        return arr[start];
+    }
+    int mid=start+(last-start)/2;
+    int ls=max_sum(arr,start,mid);
+    int rs=max_sum(arr,mid+1,last);
+    int cs=css(arr,start,mid,last);
+    return max({ls,rs,cs});
+}
+using namespace std;
+int main()
+
+{
+    cout<<"Enter the Length of the array: ";
+    int num;
+    cin>>num;
+    vector <int> arr(num,0);
+
+    for(int i=0;i<num;i++)
+    {
+        cin>>arr[i];
+    }
+    int ans=max_sum(arr,0,num-1);
+    cout<<ans;
+
     return 0;
 }

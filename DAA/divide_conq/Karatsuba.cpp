e35cd48ce651ea3a9c014b597a9ee39b5e6e3b34
Yatheendra-R <yatheendra.r07@gmail.com>
2026-03-04 15:39:37 +0530

    Updation Better way of finding the length of the number

--- DAA/divide_conq/Karatsuba.cpp
+++ DAA/divide_conq/Karatsuba.cpp
@@ -1,23 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-long length(long n)
-{
-    long cnt=0;
-    while(n>0)
-    {
-        cnt++;
-        n=n/10;
-    }
-    return cnt;
-}
-long kar(long n1,long n2)
+long kas(long n1,long n2)
 {
     if(n1<10 || n2<10)
     {
         return n1*n2;
+
     }
-    long l1=length(n1);
-    long l2=length(n2);
+
+    long l1;
+    if(n1 == 0)
+        l1 = 1;
+    else
+        l1 = log10(abs(n1)) + 1;
+
+    long l2;
+    if(n2 == 0)
+        l2 = 1;
+    else
+        l2 = log10(abs(n2)) + 1;
+    /*
+    log10(n)=x
+    To what power must 10 be raised to get n
+    10^x=n
+    log10​(1000)=3
+    10^3=1000
+    len(1000)=3+1=4
+
+
+    log10(123)≈2.0899
+    10^2.0899≈123
+    len(123)=2+1=3
+
+    digits = (long)floor(log10(abs(n))) + 1;
+    long digits = log10(n) + 1;
+    That’s because when assigning double to long:
+        C++ truncates decimal part
+        Which behaves like floor for positive numbers
+    */
+
+
+
     if(l1%2!=0)
     {
         l1++;
@@ -26,28 +49,35 @@ long kar(long n1,long n2)
     {
         l2++;
     }
-    long h1=(long)(pow(10,l1/2));
-    long h2=(long)(pow(10,l2/2));
-    long h=max(h1,h2);
     long l=max(l1,l2);
 
-    long a=(long)(n1/h);
-    long b=(long)(n1%h);
-    long c=(long)(n2/h);
-    long d=(long)(n2%h);
-    long ac=kar(a,c);
-    long bd=kar(b,d);
-    long x=kar(a+b,c+d)-(ac+bd);
-    long anns=ac*(long)pow(10,2*(l/2))+bd+x*(long)pow(10,l/2);
-    return anns;
+    long h1=(long)pow(10,l1/2);
+    long h2=(long)pow(10,l2/2);
+    long h=max(h1,h2);
+
+    long a=(long)n1/h;
+    long b=(long)n1%h;
+    long c=(long)n2/h;
+    long d=(long)n2%h;
+
+    long ac=kas(a,c);
+    long bd=kas(b,d);
+
+    long ad_bc=kas(a+b, c+d)-ac-bd;  //(a+b)*(c+d)=ac+ad+bc+bd
+    //ac+ad+bc+bd-ac-bd=ad+bc  // if i call separate two function call for ad and cb , the time complexity will increase
+
+    return ac*(long)pow(10,l)+ad_bc*(long)pow(10,l/2)+bd;
 }
 int main()
 {
     long n1;
     long n2;
+    cout<<"Enter the number: ";
     cin>>n1;
+    cout<<"Enter the number: ";
     cin>>n2;
-    long ans=kar(n1,n2);
+
+    long ans=kas(n1,n2);
     cout<<ans;
     return 0;
 }

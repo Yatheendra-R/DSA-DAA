5f98b4408e76ffb52e7a60b674f92a03c716a0e1
Yatheendra-R <yatheendra.r07@gmail.com>
2026-03-05 20:49:38 +0530

    TSP

--- DAA/DP/TSP.cpp
+++ DAA/DP/TSP.cpp
@@ -1,57 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 1e9;
-
-int n;
-vector<vector<int>> dist;
-vector<vector<int>> dp;
-
-// TSP using DP + Bitmask
-int tsp(int mask, int pos)
+int Tsp(int mask,int pos,int number_city,vector<vector<int>> &dist,vector<vector<int>> &dp)
 {
-    // If all cities are visited
-    if(mask == (1 << n) - 1)
-        return dist[pos][0];  // return to starting city
-
-    // If already computed
-    if(dp[mask][pos] != -1)
-        return dp[mask][pos];
-
-    int ans = INF;
-
-    // Try all cities
-    for(int city = 0; city < n; city++)
+    if(mask==(1<<number_city)-1)
     {
-        // If city not visited
-        if((mask & (1 << city)) == 0)
-        {
-            int newAns = dist[pos][city] +
-                         tsp(mask | (1 << city), city);
-
-            ans = min(ans, newAns);
-        }
+        return dist[pos][0];
     }
+    if(dp[mask][pos]!=-1)
+    {
+        return dp[mask][pos];
+    }
+    int ans=INT_MAX;
+    for(int city=0;city<number_city;city++)
+    {
+        if((mask&(1<<city))==0)
+        {
+            int newanswer=dist[pos][city]+Tsp(mask|(1<<city),city,number_city,dist,dp);
+            ans=min(ans,newanswer);
 
-    return dp[mask][pos] = ans;
+        }
+
+
+    }
+    return dp[mask][pos]=ans;
 }
 
 int main()
 {
-    cout << "Enter number of cities: ";
-    cin >> n;
 
-    dist.assign(n, vector<int>(n));
-    dp.assign(1 << n, vector<int>(n, -1));
+    int number_city;
+    cout<<"Enter the number of cities: ";
+    cin>>number_city;
 
-    cout << "Enter distance matrix:\n";
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            cin >> dist[i][j];
+    vector<vector<int>> dist(number_city,vector<int>(number_city));
+    vector<vector<int>> dp(1<<number_city,vector<int>(number_city,-1));
 
-    int result = tsp(1, 0);  // Start from city 0
+    for(int i=0;i<number_city;i++)
+    {
+        for(int j=0;j<number_city;j++)
+        {
+            if(i==j)
+            {
+                dist[i][j]=0;
 
-    cout << "\nMinimum travelling cost: " << result << endl;
+            }
+            else
+            {
+                cout<<"Enter the distance from city "<<i+1<<" to city "<<j+1<<" : ";
+                cin>>dist[i][j];
+            }
+
+
+        }
+    }
+
+    cout<<Tsp(1,0,number_city,dist,dp)<<endl;// mask(starting from city of index 1),pos(index),number_city,dist,dp;
+
 
     return 0;
 }

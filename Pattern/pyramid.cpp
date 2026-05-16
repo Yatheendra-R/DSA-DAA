3ca06cf3b79f61cf52486c1d6e1a80bbb08bbbb9
Yatheendra-R <yatheendra.r07@gmail.com>
2026-05-16 23:05:59 +0530

    Pyramid type patterns

---
+++ Pattern/pyramid.cpp
@@ -0,0 +1,190 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+      *
+    * * *
+  * * * * *
+* * * * * * *
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of lines(rows): ";
+    cin>>n;
+    int c=2*(n-1)+1;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(j<=c/2)
+            {
+                if(i+j>=(n-1))
+                {
+                    cout<<"* ";
+
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+            else
+            {
+                if(j-i<=c/2)
+                {
+                    cout<<"* ";
+
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+*/
+
+
+
+
+
+
+/*
+* * * * * * *
+  * * * * *
+    * * *
+      *
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of lines(rows): ";
+    cin>>n;
+    int c=2*(n-1)+1;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(j<=c/2)
+            {
+                if(i<=j)
+                {
+                    cout<<"* ";
+
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+            else
+            {
+                if(j+i<=c-1)
+                {
+                    cout<<"* ";
+
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+
+
+/*
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+int main(){
+
+    int n;
+    cout<<"Enter the number of lines(rows): ";
+    cin>>n;
+    int c=2*(n-1)+1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(j<=c/2)
+            {
+                if(i+j>=(n-1))
+                {
+                    cout<<"* ";
+
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+            else
+            {
+                if(j-i<=c/2)
+                {
+                    cout<<"* ";
+
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(j<=c/2)
+            {
+                if(i<=j)
+                {
+                    cout<<"* ";
+
+                }
+                //(i+j>=)
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+            else
+            {
+                if(j+i<=c-1)
+                {
+                    cout<<"* ";
+
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+*/
+

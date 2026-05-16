7eeae5c52d0659e241869f4c9a7237b8cd621e76
Yatheendra-R <yatheendra.r07@gmail.com>
2026-05-16 20:20:22 +0530

    Pattern

---
+++ Pattern/basic.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+****
+****
+****
+****
+int main()
+{
+
+    int r;
+    int c;
+    cout<<"Enter the row and column: "<<endl;
+    cin>>r>>c;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+*/

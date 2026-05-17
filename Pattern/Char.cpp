d9f1ec4da75df2732f78d16c2e62fb087cc17a6d
Yatheendra-R <yatheendra.r07@gmail.com>
2026-05-17 13:20:49 +0530

    Pattern based on Char

---
+++ Pattern/Char.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+A
+A B
+A B C
+A B C D
+A B C D E
+int main()
+{
+    int n;
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        char a='A';
+        for(int j=0;i>=j;j++)
+        {
+            cout<<a<<" ";
+            a++;
+        }
+        cout<<endl;
+
+    }
+        return 0;
+}
+
+*/
+/*
+A
+B C
+D E F
+G H I J
+K L M N O
+int main()
+{
+    int n;
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+    char a='A';
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;i>=j;j++)
+        {
+            cout<<a<<" ";
+            a++;
+        }
+        cout<<endl;
+
+    }
+        return 0;
+}
+*/
+/*
+A
+B B
+C C C
+D D D D
+E E E E E
+int main()
+{
+    int n;
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+    char a='A';
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;i>=j;j++)
+        {
+            cout<<a<<" ";
+        }
+        a++;
+
+        cout<<endl;
+
+    }
+        return 0;
+}*/
+
+
+/*
+A B C D E
+A B C D
+A B C
+A B
+A
+int main()
+{
+
+    int n;
+    cout<<"Enter the number of lines: ";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        char a='A';
+        for(int j=0;j<n;j++)
+        {
+            if(i+j<=(n-1))
+            {
+                cout<<a<<" ";
+                a++;
+            }
+
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+*/
+
+
+
+int main()
+{
+    return 0;
+}

faf524629979851197247881ecffe4db977437a2
Yatheendra-R <yatheendra.r07@gmail.com>
2026-05-17 15:38:18 +0530

    added few more Unique pattern

--- Pattern/unique.cpp
+++ Pattern/unique.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 
 /*
 1
@@ -80,3 +82,179 @@ int main()
     return 0;
 }
 */
+
+/*
+* * * * * * * * * *
+* * * *     * * * *
+* * *         * * *
+* *             * *
+*                 *
+*                 *
+* *             * *
+* * *         * * *
+* * * *     * * * *
+* * * * * * * * * *
+int main()
+{
+    int n;
+    cout<<"Enter the number of the line: ";
+    cin>>n;
+    int c=n/2;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i+j<c)
+            {
+                cout<<"* ";
+            }
+            else if(j-i>=c)
+            {
+                cout<<"* ";
+
+            }
+            else if(i-j>=c)
+            {
+                cout<<"* ";
+            }
+            else if(i+j>=(c+n-1))
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+*/
+
+/*
+*                   *
+* *               * *
+* * *           * * *
+* * * *       * * * *
+* * * * *   * * * * *
+* * * * * * * * * * *
+* * * *       * * * *
+* * *           * * *
+* *               * *
+*                   *
+
+int main()
+{
+    int r;
+    cout<<"Enter the number of the line: ";
+    cin>>r; //9
+    int c=r+1; // 10
+    int hr=r/2; //4
+    int hc=c/2; //5
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(j<hc&&i>hr)
+            {
+                if(i+j<(c-1))
+                {
+                    cout<<"* ";
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+            else if(i<=hr&&j<hc)
+            {
+                if(i>=j)
+                {
+                    cout<<"* ";
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+            else if(i>hr&&j>=hc)
+            {
+                if(j-i>0)
+                {
+                    cout<<"* ";
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+            else if(i<=hr&&j>=hc)
+            {
+                if(i+j>=(c-1))
+                {
+                    cout<<"* ";
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+
+            }
+
+
+
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+*/
+int main()
+{
+    //int r;
+    //cout<<"Enter the number of the line: ";
+    //cin>>r; //9
+
+    //int c=r+1; // 10
+    //int hr=r/2; //4
+    //int hc=c/2; //5
+    int n=7;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==0 || j==0||i==n-1||j==n-1)
+            {
+                cout<<"4 ";
+            }
+            else if(i==1 || j==1 || i==n-2 || j==n-2)
+            {
+                cout<<"3 ";
+            }
+            else if(i==2 || j==2 || i==n-3 || j==n-3)
+            {
+                cout<<"2 ";
+            }
+            else if(i==3 || j==3 || i==n-4 || j==n-4)
+            {
+                cout<<"1 ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+
+
+
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
